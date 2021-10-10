2216c1e5facd3d23ad22e1feae16e26ba1dbf5d3
nuel1 <nwekejoseph52@gmail.com>
2021-10-10 01:07:18 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+!function () {
+  "use strict";
+  let f, p;
+  f = (function () {
+    return function (a) {
+      console.log(a);
+      if (a[0].isIntersecting) {
+        document.documentElement.style.setProperty("--b-blur", "0");
+      }
+    };
+  })();
+
+  p = function() {
+    return {
+      root: null,
+      threshold: 0.5,
+      rootMargin: '-200px'
+    }
+  }();
+
+  console.log(f);
+
+  const observer = new IntersectionObserver(f, p);
+  const screen = document.querySelector('.screen--cover')
+  observer.observe(screen);
+
+}();
